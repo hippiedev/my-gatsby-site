ff47f67f8bcf880720cfd460697b990fdd591974
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2021-04-24 18:05:05 +0200

    chore: fixed filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/src/blog/`,
+      },
+      __key: "content",
+    },
+  ],
 };
